4e5e080a5f3d292a366a32b72fa9d157ca7c6413
jdavidIP <ibapol2003@gmail.com>
2025-07-14 11:03:30 -0400

    first commit

---
+++ vue-tutorial/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings :limit="3" :show-button="true" />
+</template>
